5352858d83e9514334b389622029f5e99e0fa1d1
Arish Khan <aarish0090@gmail.com>
2026-07-23 02:25:08 +0530

    Add header for member page

--- gms-frontend/src/pages/GeneralUser/GeneralUser.js
+++ gms-frontend/src/pages/GeneralUser/GeneralUser.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Link } from 'react-router-dom';
 import Membercard from "../../components/Membercard/Membercard";
+import { useEffect } from 'react';
 
 const GeneralUser = () => {
+
+  const [header,setHeader]=useState("");
+
+  useEffect(()=>{
+    const func = sessionStorage.getItem('func');
+    functionCall(func);
+
+  },[])
+    
+  const functionCall = async(func)=>{
+    switch(func){
+      case "monthlyjoined":
+      setHeader("Monthly Joined Members")
+      break;
+
+      case "3daysexpire":
+      setHeader("Expiring in 3 days Members")
+      break;
+
+       case "4daysexpire":
+      setHeader("Expiring in 4-7 days Members")
+      break;
+
+      case "expired":
+      setHeader("Expired Members")
+      break;
+
+      case "inactive":
+      setHeader("Inactive Members")
+      break;
+    }
+  }
   return (
     <div className='text-black p-5 w-3/4 flex-col'>
         <div className='border-2 bg-slate-900 flex justify-between w-full text-white rounded-lg p-3'>
@@ -14,7 +47,7 @@ const GeneralUser = () => {
         </div>
 
         <div className='mt-5 text-xl text-slate-900'>
-            {"5 Monthly Joined Members"}
+            {header}
 
         </div>
         <div className='bg-slate-100 p-5 mt-5 rounded-lg grid gap-2 grid-cols-3 overflow-x-auto h-[80%]'>
